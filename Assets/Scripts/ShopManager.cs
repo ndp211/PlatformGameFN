d6d98159d6a942c682bba2939c14b7e2d5b751f9
ndp211 <111591996+ndp211@users.noreply.github.com>
2023-06-25 01:50:04 +0700

    Final

---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NDP.PlatformGame
+{
+    public class ShopManager : Singleton<ShopManager>
+    {
+        public ShopItem[] items;
+    }
+
+}
